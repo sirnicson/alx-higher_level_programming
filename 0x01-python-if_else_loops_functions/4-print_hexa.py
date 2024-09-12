09eb893194473f2a123addf1751b118f599872fe
Nicholas W <sirnicson@example.com>
2024-09-12 22:42:48 +0300

    Update 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# Create the formatted output for numbers 0 to 99 in hexadecimal
-output = "\n".join("{} = {}".format(i, hex(i)) for i in range(100))
+for i in range(100):  # Numbers from 0 to 99
+    if i > 0:
+        print(" ", end="")  # Add a space before each new line except the first
+    print("{} = {}".format(i, hex(i)), end="")
 
-# Print the result in one go to avoid extra newlines
-print(output)
