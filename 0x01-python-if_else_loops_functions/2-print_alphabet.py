28a761a094c84a37eaea7fd8c806342ee2e837e6
Nicholas W <sirnicson@example.com>
2024-09-12 21:42:36 +0300

    Update task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):  # ASCII values for 'a' to 'z'
-    print(chr(i), end="")
+alphabet = ''.join(chr(i) for i in range(97, 123))
+print("{}".format(alphabet))
+
