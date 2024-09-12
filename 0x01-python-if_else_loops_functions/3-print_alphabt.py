22f3e18766962c6427a63f03a6a75e8dab020353
Nicholas W <sirnicson@example.com>
2024-09-12 22:10:23 +0300

    Update task 3 again

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
-print("{}".format(''.join(chr(i) for i in range(97, 123) if chr(i) not in 'eq')), end="")
+alphabet = ''.join(
+    chr(i) for i in range(97, 123) if chr(i) not in 'eq'
+)
+
+print("{}".format(alphabet), end="")
+
