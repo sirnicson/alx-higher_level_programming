ec7aae1a422c6ad2ec5cbe9eb6f9b89fce11b1c3
sirnicson <sirnicson.nicson@gmail.com>
2024-09-22 16:07:36 +0300

    Update repo

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(''.join(f"{chr(122 - i).lower() if i % 2 == 0 else chr(90 - i)}" for i in range(26)), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
