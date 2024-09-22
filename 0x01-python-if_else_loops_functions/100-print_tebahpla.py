3cdc182802703e547064e6006dcce6f776a8c549
sirnicson <sirnicson.nicson@gmail.com>
2024-09-22 15:59:04 +0300

    advance 2

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+print(''.join(f"{chr(122 - i).lower() if i % 2 == 0 else chr(90 - i)}" for i in range(26)), end='')
