8f17fa3f5f5eda79bbc8b870b4d86a18f85b0523
sirnicson <sirnicson.nicson@gmail.com>
2024-09-22 15:11:45 +0300

    Update cpython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -12,15 +12,19 @@ void print_python_list(PyObject *p)
         return;
     }
 
-    Py_ssize_t size = PyList_Size(p);
+    // Get the size of the list without using Py_SIZE
+    Py_ssize_t size = 0;
+    PyObject *item;
+    for (size = 0; PyList_Check(p) && (item = PyList_GetItem(p, size)) != NULL; size++)
+        ;
+
     printf("[*] Python list info\n");
     printf("[*] Size of the list: %zd\n", size);
-    printf("[*] Allocated: %zd\n", ((PyListObject *)p)->allocated);
+    printf("[*] Allocated: %zd\n", ((PyListObject *)p)->allocated); // Using allocated directly
 
     for (Py_ssize_t i = 0; i < size; i++)
     {
-        PyObject *item = PyList_GetItem(p, i);
-        const char *type_name = Py_TYPE(item)->tp_name;
+        const char *type_name = Py_TYPE(PyList_GetItem(p, i))->tp_name;
         printf("Element %zd: %s\n", i, type_name);
     }
 }
@@ -37,13 +41,20 @@ void print_python_bytes(PyObject *p)
         return;
     }
 
-    Py_ssize_t size = PyBytes_Size(p);
+    // Get the size of the bytes object without using PyBytes_GET_SIZE
+    Py_ssize_t size = 0;
+    while (PyBytes_Check(p) && (PyBytes_AsString(p)[size] != '\0'))
+        size++;
+
     printf("[*] Python bytes info\n");
     printf("[*] Size of the bytes: %zd\n", size);
     printf("[*] First %zd bytes: ", size > 10 ? 10 : size);
+
+    // Print up to the first 10 bytes
     for (Py_ssize_t i = 0; i < (size > 10 ? 10 : size); i++)
     {
         printf("%02x ", (unsigned char)PyBytes_AsString(p)[i]);
     }
     printf("\n");
 }
+
