0829b0bc2a362d04bb64406f620c77f9848f6da8
sirnicson <sirnicson.nicson@gmail.com>
2024-10-14 22:42:54 +0300

    Implemented task 4 & test file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    
+
     for i in range(list_length):
         try:
             # Check if the index exists in both lists
             a = my_list_1[i]
             b = my_list_2[i]
-            
+
             # Attempt division
             result = a / b
-            
+
         except IndexError:
             print("out of range")
             result = 0  # Out of range, set result to 0
-            
+
         except TypeError:
             print("wrong type")
             result = 0  # Wrong type, set result to 0
-            
+
         except ZeroDivisionError:
             print("division by 0")
             result = 0  # Division by zero, set result to 0
-            
+
         finally:
             # Append the result (or 0 in case of an error) to the new list
             new_list.append(result)
-    
+
     return new_list
