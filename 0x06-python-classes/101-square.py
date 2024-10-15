3d62e21bd3046c5c625b9edb851c31fa24b1fff3
sirnicson <sirnicson.nicson@gmail.com>
2024-10-15 10:49:44 +0300

    Implemented task 8 & test file

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""Create Square class."""
+
+
+class Square:
+    """Define a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square.
+
+        Args:
+            size (int): The size of the square.
+            position (tuple): The position of the square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character # at the specified position."""
+        if self.__size == 0:
+            print()
+            return
+        for _ in range(self.__position[1]):
+            print()
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Return the string representation of the square."""
+        output = ""
+        if self.__size == 0:
+            return output
+        for _ in range(self.__position[1]):
+            output += "\n"
+        for _ in range(self.__size):
+            output += " " * self.__position[0]
+            output += "#" * self.__size + "\n"
+        return output[:-1]  # Remove the last newline character
