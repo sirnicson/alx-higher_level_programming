0ca43d4e129abdce1cb501feee843ebbf055aac9
sirnicson <sirnicson.nicson@gmail.com>
2024-10-15 10:33:50 +0300

    Implemented task 5 & test file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Creating a square class """
+
+
+class Square:
+    """ A class that defines a square with a private attribute size """
+
+    def __init__(self, size=0):
+        """ Initializes the square with an optional size """
+        self.size = size  # Use the setter to validate the size
+
+    @property
+    def size(self):
+        """ Property getter to retrieve the size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Property setter to set the size with validation """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value  # Private instance attribute
+
+    def area(self):
+        """ Returns the current square area """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ Prints the square using the '#' character """
+        if self.__size == 0:
+            print()  # Print an empty line if size is 0
+        else:
+            for _ in range(self.__size):
+                print('#' * self.__size)  # Print each row of the square
