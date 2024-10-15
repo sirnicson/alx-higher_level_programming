fd665cf5afa8100105962bb3552fa72747e713ba
sirnicson <sirnicson.nicson@gmail.com>
2024-10-15 10:25:01 +0300

    Implemented task 2 & test file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Creating a square class """
+
+
+class Square:
+    """ A class that defines a square with a private attribute size """
+
+    def __init__(self, size=0):
+        """ Initializes the square with an optional size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Private instance attribute
