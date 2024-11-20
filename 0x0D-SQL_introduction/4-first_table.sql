a96d15fc4b445a57ec942da12a4cd93b2f7018c3
sirnicson <sirnicson.nicson@gmail.com>
2024-11-20 11:47:48 +0300

    Task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,6 @@
+-- Script to create first_table in the specified database
+-- If first_table already exists, the script will not fail
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
