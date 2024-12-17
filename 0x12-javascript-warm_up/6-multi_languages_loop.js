49b4f88e08d75ec5ea18f4c7d83bb0e338659f84
sirnicson <sirnicson.nicson@gmail.com>
2024-12-18 00:56:57 +0300

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* prints 3 lines using an array of string and a loop */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
