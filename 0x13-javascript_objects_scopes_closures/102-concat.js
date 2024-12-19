086793a700d7276fda29020e545a3f5c87c9594b
sirnicson <sirnicson.nicson@gmail.com>
2024-12-19 18:24:25 +0300

    Task final

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,9 +3,25 @@
 
 const fs = require('fs');
 
-const [source1, source2, destination] = process.argv.slice(2);
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js <file1> <file2> <destination>');
+  process.exit(1);
+}
 
-const content1 = fs.readFileSync(source1, 'utf8');
-const content2 = fs.readFileSync(source2, 'utf8');
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const destination = process.argv[4];
 
-fs.writeFileSync(destination, content1 + content2);
+try {
+  
+  const content1 = fs.readFileSync(file1, 'utf8');
+  const content2 = fs.readFileSync(file2, 'utf8');
+  
+  
+  fs.writeFileSync(destination, content1 + content2);
+
+  console.log('Files concatenated successfully!');
+} catch (error) {
+  console.error(`Error: ${error.message}`);
+  process.exit(1);
+}
